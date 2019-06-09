e5a7c632ee02b88cd611439b22a193ad82718a7f
root <andreyklimkin@yandex.ru>
2019-06-09 12:34:01 +0000

    Last version of Coursework

--- src/batch_generator.py
+++ src/batch_generator.py
@@ -8,6 +8,7 @@ from torch.utils.data.dataloader import DataLoader
 
 from skimage import img_as_float, io
 from skimage.transform import resize
+from bisect import bisect_left, bisect_right
 
 from tqdm import tqdm
 
@@ -29,7 +30,8 @@ class WraptorDataLoader(DataLoader):
 
 
 class BatchGenerator(Dataset):
-    def __init__(self, img_dirs, is_train=True, need_crop=True, crop_size=256, need_resize=True, resize_shape=(256, 256, 3)):
+    def __init__(self, img_dirs, is_train=True, need_crop=True, use_as_first_only_first=True, max_frames_ahead=10,
+                 back_shift_range=(1, 1), crop_size=256, need_resize=True, resize_shape=(256, 256, 3)):
         self.pairs_I0Ik_paths = []
         self.need_crop = need_crop
         self.crop_size = crop_size
@@ -37,25 +39,40 @@ class BatchGenerator(Dataset):
         self.need_resize = need_resize
         self.resize_shape = resize_shape
         self.is_train = is_train
-        
-        
+        self.back_shift_range = back_shift_range
+        self.start_video_idx = []
+        get_frames = 0
         for dir_path in tqdm(img_dirs):
-            I0_path = "%s/%05d.jpg" % (dir_path, 0)
-            for i in range(len(glob(dir_path + "/*"))):
-                Ik_path = "%s/%05d.jpg" % (dir_path, i)
-                self.pairs_I0Ik_paths.append((I0_path, Ik_path))
+            if use_as_first_only_first:
+                self.start_video_idx.append(get_frames)
+                I0_path = "%s/%05d.jpg" % (dir_path, 0)
+                for i in range(len(glob(dir_path + "/*"))):
+                    Ik_path = "%s/%05d.jpg" % (dir_path, i)
+                    self.pairs_I0Ik_paths.append((I0_path, Ik_path))
+                    get_frames += 1
+            else:
+                for first_frame_idx in range(0, len(glob(dir_path + "/*")) - 1):
+                    self.start_video_idx.append(get_frames)
+                    I0_path = "%s/%05d.jpg" % (dir_path, first_frame_idx)
+                    for i in range(first_frame_idx + 1, min(len(glob(dir_path + "/*")), first_frame_idx + max_frames_ahead + 1)):
+                        Ik_path = "%s/%05d.jpg" % (dir_path, i)
+                        self.pairs_I0Ik_paths.append((I0_path, Ik_path))
+                        get_frames += 1
         
     
     def __len__(self):
         return len(self.pairs_I0Ik_paths)
 
     def __getitem__(self, idx):
+        video_start_frame_idx = self.start_video_idx[bisect_right(self.start_video_idx, idx) - 1]
         I0 = img_as_float(io.imread(self.pairs_I0Ik_paths[idx][0]))
         Ik = img_as_float(io.imread(self.pairs_I0Ik_paths[idx][1]))
         if np.sum((I0 - Ik) ** 2) < 1e-6:
             Ik_1 = I0.copy()
         else:
-            Ik_1 = img_as_float(io.imread(self.pairs_I0Ik_paths[idx - 1][1]))
+            prev_index = max(video_start_frame_idx, idx - np.random.randint(self.back_shift_range[0], 
+                                                                            self.back_shift_range[1] + 1))
+            Ik_1 = img_as_float(io.imread(self.pairs_I0Ik_paths[prev_index][1]))
         
         if self.is_train:
             if self.need_crop:
@@ -69,5 +86,5 @@ class BatchGenerator(Dataset):
                 I0 = resize(I0, self.resize_shape)
                 Ik_1 = resize(Ik_1, self.resize_shape)
                 Ik = resize(Ik, self.resize_shape)
-            
+        #print(np.mean((I0 - Ik) ** 2))
         return (I0.astype(np.double), Ik_1.astype(np.double), Ik.astype(np.double))
--- src/train_utils.py
+++ src/train_utils.py
@@ -9,7 +9,7 @@ from collections import OrderedDict
 from color_utils import rgb2lab_torch, lab2rgb_torch
 from glob import glob
 from os import path, mkdir
-from skimage import img_as_ubyte, io
+from skimage import img_as_ubyte, io, img_as_float
 from skimage.transform import resize
 from skimage.measure import compare_psnr
 from skimage.color import rgb2lab, lab2rgb
@@ -40,7 +40,7 @@ def rgb2gray(rgb):
 
 
 def infer_batch(batch, refinement_net, local_transfer_net, global_transferer,
-                use_only_local=False, verbose=False):
+                use_only_local=False, zero_global=False,  verbose=False):
     I1_lab = rgb2lab_torch(batch[0].cuda(), use_gpu=True)
     Ik_lab = rgb2lab_torch(batch[2].cuda(), use_gpu=True)
 
@@ -60,6 +60,8 @@ def infer_batch(batch, refinement_net, local_transfer_net, global_transferer,
     
     t = time()
     global_batch_output = global_transferer.forward(G1_array, Gk_array, batch[0].cpu().numpy())
+    if zero_global:
+        global_batch_output *= 0
     if verbose:
         print("Global inference time %.3f" % (time() - t))
     global_batch_output = torch.tensor(global_batch_output.transpose(0, 3, 1, 2), dtype=torch.double, requires_grad=False)
@@ -68,7 +70,9 @@ def infer_batch(batch, refinement_net, local_transfer_net, global_transferer,
     stacked_input_refinement = torch.cat([Gk_tensor, local_batch_output_lab, global_batch_output_lab.cuda()], dim=1)
     if use_only_local:
         refinement_output_lab = local_batch_output_lab.permute(0, 2, 3, 1)
-        result_lab = refinement_output_lab
+        gt_l = Ik_lab[..., 0][..., None]
+        predicted_ab = refinement_output_lab[..., 1:]
+        result_lab = torch.cat((gt_l, predicted_ab), dim=3)
         result_rgb = lab2rgb_torch(result_lab, use_gpu=True).cpu()
     else:
         refinement_output_lab = refinement_net(stacked_input_refinement).permute(0, 2, 3, 1)
@@ -77,7 +81,7 @@ def infer_batch(batch, refinement_net, local_transfer_net, global_transferer,
         result_lab = torch.cat((gt_l, predicted_ab), dim=3)
         result_rgb = lab2rgb_torch(result_lab, use_gpu=True).cpu()
     
-    return (refinement_output_lab, result_rgb, 
+    return (result_lab, result_rgb, 
             local_batch_output.permute(0, 2, 3, 1), global_batch_output.permute(0, 2, 3, 1))
 
 
@@ -85,21 +89,25 @@ def frame_to_tensor(frame):
     return torch.tensor(frame, dtype=torch.double, requires_grad=False).cuda()
 
 
-def load_video_frames(path, resize_size=(128, 128, 3), anti_aliasing=False):
+def load_video_frames(path, resize_size=(128, 128, 3), resize_type="resize", left_crop_point=None, anti_aliasing=False):
     frames = []
     frames_num = len(glob(path + "/*"))
     for i in range(frames_num):
         frame = io.imread("%s/%05d.jpg" % (path, i))
-        frames.append(resize(frame, resize_size, anti_aliasing=anti_aliasing))
+        if resize_type == "resize":
+            frames.append(resize(frame, resize_size, anti_aliasing=anti_aliasing))
+        if resize_type == "crop":
+            frames.append(img_as_float(frame[left_crop_point[0]: left_crop_point[0] + resize_size[0],
+                                left_crop_point[1]: left_crop_point[1] + resize_size[1], :]))
     return frames
 
 def load_all_test_videos(video_pathes, resize_size=(128, 128, 3), anti_aliasing=False):
     test_video_frames = []
     for video_path in video_pathes:
-        test_video_frames.append(load_video_frames(video_path, resize_size, anti_aliasing))
+        test_video_frames.append(load_video_frames(video_path, resize_size, anti_aliasing=anti_aliasing))
     return test_video_frames
 
-def inference_test_video(frames, refinement_net, local_transfer_net, global_transferer, use_only_local=False):
+def inference_test_video(frames, refinement_net, local_transfer_net, global_transferer, use_only_local=False, zero_global=False):
     I0 = frames[0]
     I_prev = frames[0]
     Gk_1 = rgb2gray(I_prev)
@@ -114,13 +122,38 @@ def inference_test_video(frames, refinement_net, local_transfer_net, global_tran
                  frame_to_tensor(I_prev[None, ...]),
                  frame_to_tensor(cur_frame[None, ...]))
         _, result_rgb, result_local, result_global = infer_batch(batch, refinement_net, local_transfer_net, 
-                                                                 global_transferer, use_only_local)
+                                                                 global_transferer, use_only_local, zero_global)
         output_rgb_frames.append(result_rgb[0].detach().cpu().numpy())
         output_local.append(result_local[0].detach().cpu().numpy())
         output_global.append(result_global[0].detach().cpu().numpy())
         I_prev = output_rgb_frames[-1]
         
     return gray_frames, output_rgb_frames, output_local, output_global
+
+# def inference_train_video(frames, refinement_net, local_transfer_net, global_transferer, 
+#                           use_only_local=False, zero_global=False):
+#     I0 = frames[0]
+#     I_prev = frames[0]
+#     Gk_1 = rgb2gray(I_prev)
+#     G0 = Gk_1.copy()
+#     output_rgb_frames = [I0]
+#     output_lab_frames = [rgb2lab(I0)]
+#     output_local = [I0]
+#     output_global = [I0]
+#     gray_frames = [G0]
+#     for cur_frame in tqdm(frames[1:]):
+#         gray_frames.append(rgb2gray(cur_frame))
+#         batch = (frame_to_tensor(I0[None, ...]), 
+#                  frame_to_tensor(I_prev[None, ...]),
+#                  frame_to_tensor(cur_frame[None, ...]))
+#         result_lab, result_rgb, result_local, result_global = infer_batch(batch, refinement_net, local_transfer_net, 
+#                                                                  global_transferer, use_only_local, zero_global)
+#         output_rgb_frames.append(result_rgb[0])
+#         output_local.append(result_local[0])
+#         output_global.append(result_global[0])
+#         I_prev = output_rgb_frames[-1]
+        
+#     return gray_frames, output_rgb_frames, output_local, output_global
 
 
 def make_video_from_frames(output_path, frames):
